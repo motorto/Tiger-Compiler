e03b777e790c65de35efd0268279852003560a23
André Cerqueira <acerqueira021@gmail.com>
2021-12-28 15:22:36 +0000

    CodeGen.hs: Add support for stms , expr and cond

--- src/CodeGen.hs
+++ src/CodeGen.hs
@@ -19,24 +19,12 @@ popTemp k =  modify (\(t,l) -> (t-k,l))
 newLabel :: State Count Label 
 newLabel = do (t,l)<-get; put (t,l+1); return ("L"++show l)
 
-{-
- - Traduzir Expressões 
- 
-Expr : num { Number $1 } [X]
-     | stringContent { BuildString $1 } [Opt]
-     | LValue { Var $1} [X]
-     | Expr x  Expr { Op x $1 $3 } [X]
-     | Expr x Expr { Cond x $1 $3 }  [X] -- TransCond
-     | '-'Expr {Negative $2}  [?]
-     | identifier '(' ExprList ')' { FuncCall $1 $3} [X]
--}
-
 transExp :: Table -> Expr -> Identifier -> State Count [Instr]
 
 transExp tabl (Number n) dest
   = return [MOVEI dest n]
 
-transExp tabl (Var (Varname x) dest
+transExp tabl (Var (VarName x)) dest
   = case Map.lookup x tabl of
       Just temp -> return [MOVE dest temp]
       Nothing -> error "invalid variable"
@@ -51,50 +39,35 @@ transExp tabl (Op op e1 e2) dest
        place2 <- newTemp 
        code1 <- transExp tabl e1 place1
        code2 <- transExp tabl e2 place2
-       -- popTemp 2 -- ????
        return (code1 ++ code2 ++ [OP op dest place1 place2])
 
-
-transExp tabl (FuncCall id exps ) dest
-  = do (code1, temps) <- TransExps tabl exps
-       -- popTemp (length exps)
+transExp tabl (FuncCall id exps) dest
+  = do (code1, temps) <- transExps tabl exps
        return (code1 ++ [CALL dest id temps])
 
-transExpressions tabl exps = worker exps 
-  where
-    worker []  = return ([], [])
-    worker (expHead:expTail) 
-      = do temp1 <- newTemp 
-           code1 <- transExp tabl expHead temp1
-           (code2, temp2) <- worker expTail
-           return (code1 ++ code2, temp1:temp2)
+------ ???
+--transExp tabl (1) dest = return [MOVEI dest 1]
+--transExp tabl (0) dest = return [MOVEI dest 0]
+------
 
-{-
-   '('ExprSeq')' { ExpSeq $2 } [?]
-   if Expr then Expr {IfThen $2 $4} [X]
-   if Expr then Expr else Expr {IfThenElse $2 $4 $6} [X]
-   while Expr do Expr {While $2 $4 } [X]
-   LValue ':=' Expr {Assign $1 $3} [?]
-   break { Break } []
-   let VarDecList in ExprSeq end { LetIn $2 $4} []
- -}
+transExps::Table -> [Expr] -> State Count ([Instr],[Temp])
+transExps tabl [] = return ([],[])
+transExps tabl (expr:exprs) = do temp1 <- newTemp
+                                 code1 <- transExp tabl expr temp1
+                                 (code2,temps) <- transExps tabl exprs
+                                 return (code1 ++ code2,[temp1] ++ temps)
 
-transStat :: Table -> Stm -> State Count [Instr]
-transStat (ExpSeq stat1:statTail)
-      = do code1 <- transExp tabl stat1 
-           code2 <- transExp tabl statTail
-           return (code1 ++ code2)
+transStat :: Table -> Expr -> State Count [Instr]
+transStat tabl (IfThen cond stat1)
+  = do label1 <- newLabel 
+       label2 <- newLabel 
+       code1 <- transCond tabl cond label1 label2
+       code2 <- transStat tabl stat1
+       return (code1 ++ [LABEL label1] ++
+                code2 ++ [LABEL label2])
 
-transStat (IfThen cond stat1)
-      = do label1 <- newLabel 
-        label2 <- newLabel 
-        code1 <- transCond tabl cond label1 label2
-        code2 <- transStat tabl stat1
-        return (code1 ++ [LABEL label1] ++
-                code2 ++ [LABEL false])
-
-transStat (IfThenElse cond stat1 stat2)
-      = do label1 <- newLabel 
+transStat tabl (IfThenElse cond stat1 stat2)
+   = do label1 <- newLabel 
         label2 <- newLabel 
         label3 <- newLabel 
         code1 <- transCond tabl cond label1 label2
@@ -104,37 +77,67 @@ transStat (IfThenElse cond stat1 stat2)
                 code2 ++ [JUMP label3 , LABEL label2] ++ 
                 code3 ++ [LABEL label3])
 
-transStat (While cond stat1)
-      = do label1 <- newLabel 
-        label2 <- newLabel 
-        label3 <- newLabel
+transStat tabl (While cond stat1)
+   = do label1 <- newLabel  --true
+        label2 <- newLabel  -- loop
+        label3 <- newLabel  -- false
         code1 <- transCond tabl cond label2 label3
         code2 <- transStat tabl stat1
         return ([LABEL label1] ++ code1 ++
                 [LABEL label2] ++ code2 ++ 
-                [JUMP label, LABEL label3])
+                [JUMP label1, LABEL label3])
 
-transStat (Assing id exp1) 
-     = case Map.lookup id tabl of
-        Nothing -> error "undefined variable"
-        Just dest -> do temp1 <- newTemp 
-                      code1 <- transExp tabl exp1 temp1
-                      return (code1 ++ [MOVE dest temp1])
+transStat tabl (Assign (VarName id) exp1) 
+   = case Map.lookup id tabl of
+          Nothing -> error "undefined variable"
+          Just dest -> do temp1 <- newTemp 
+                          code1 <- transExp tabl exp1 temp1
+                          return (code1 ++ [MOVE dest temp1])
 
-
+transStats :: Table -> [Expr] -> State Count [Instr]
+transStats tabl [] = return []
+transStats tabl (head:tail) = do code1 <- transStat tabl head
+                                 codes <- transStats tabl tail
+                                 return (code1 ++ codes)
+                                 
 transCond :: Table -> Expr -> Label -> Label -> State Count [Instr]
-transCond tabl (Op rel e1 e2) ltrue lfalse
+
+-- --- ???
+-- transCond tabl (1) labelt labelf = return [JUMP labelt]
+-- transCond tabl (0) labelt labelf = return [JUMP labelf] 
+-- --- ???
+
+transCond tabl (Cond And cond1 cond2) ltrue lfalse
+    = do arg2 <- newLabel
+         code1 <- transCond tabl cond1 arg2 lfalse
+         code2 <- transCond tabl cond2 ltrue lfalse
+         return (code1 ++ [LABEL arg2] ++ code2)
+
+transCond tabl (Cond Or cond1 cond2) ltrue lfalse
+    = do arg2 <- newLabel
+         code1 <- transCond tabl cond1 ltrue arg2
+         code2 <- transCond tabl cond2 ltrue lfalse
+         return (code1 ++ [LABEL arg2] ++ code2)
+
+transCond tabl (Cond op exp1 exp2) ltrue lfalse
     = do t1 <- newTemp
          t2 <- newTemp
          code1 <- transExp tabl exp1 t1
          code2 <- transExp tabl exp2 t2
          return (code1 ++ code2 ++ 
-                [COND t1 rel t2 ltrue lfalse])
+                [COND t1 op t2 ltrue lfalse])
 
-transExp tabl (Cond op e1 e2) ltrue lfalse
-  = do t1 <- newTemp 
-       t2 <- newTemp 
-       code1 <- transExp tabl e1 t1
-       code2 <- transExp tabl e2 t2
-       -- popTemp 2 -- ????
-       return (code1 ++ code2 ++ [COND t1 rel t2 ltrue lfalse])
+--- ???
+transCond tabl exp1 ltrue lfalse 
+    = do t1 <- newTemp 
+         code1 <- transExp tabl exp1 t1
+         return (code1 ++  [COND t1 Equals 1 ltrue lfalse]) -- ???????? COND t != 0 
+--- ???
+
+{-
+   break { Break } []
+To handle these, the translation function for
+statements must have an extra inherited parameter which is the label that a break
+or exit statement must jump to. This attribute is changed whenever a new loop is
+entered. Before the first loop is entered, this attribute is undefined.
+-}
