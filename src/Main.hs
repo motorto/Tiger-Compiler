c17c8d6813333c007d3a51d1069a63492f3c24c2
André Cerqueira <acerqueira021@gmail.com>
2022-01-09 11:46:40 +0000

    Main: Update main to dump mips.asm file

--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,20 @@
 module Main where
 
-import Lexer
-import Parser
 import CodeGen
-
+import Control.Monad.State
 import Data.Map (Map)
 import qualified Data.Map as Map
-import Control.Monad.State
+import Lexer
+import MipsGen
+import Parser
 
 main :: IO ()
 main = do
   input <- getContents
   let ast = parser (alexScanTokens input)
   writeFile "ast.txt" $ show ast ++ "\n"
-  let code = evalState (transProgram ast) (0,0)
-  print code
+  let code = evalState (transProgram ast) (0, 0)
+  writeFile "3addr.txt" $ show code ++ "\n"
+  let mips = printPlease $ start code
+  writeFile "mips.asm" mips
+  putStr mips
